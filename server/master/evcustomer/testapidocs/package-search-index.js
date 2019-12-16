89446155bc7dfb14d693d0289df6f0bdbc5d9e2b
Travis CI User <travis@example.org>
2019-12-16 16:00:43 +0000

    Updated javadocs for branch master

---
+++ server/master/evcustomer/testapidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.powertac.common"},{"l":"org.powertac.common.repo"},{"l":"org.powertac.customer"},{"l":"org.powertac.evcustomer"},{"l":"org.powertac.evcustomer.beans"},{"l":"org.powertac.evcustomer.customers"}]
