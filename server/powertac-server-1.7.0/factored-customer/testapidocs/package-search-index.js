0a62fdb8248097a86ad9f8f5ccea5dddc201dfda
Travis CI User <travis@example.org>
2020-03-25 16:55:43 +0000

    Updated javadocs for branch powertac-server-1.7.0

---
+++ server/powertac-server-1.7.0/factored-customer/testapidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.powertac.common"},{"l":"org.powertac.common.repo"},{"l":"org.powertac.customer"},{"l":"org.powertac.factoredcustomer"}]
